84fc8472cd0c785f92456c5f0893a4c4f1172087
yousef-indja <indja98@gmail.com>
2021-03-04 14:28:32 +0000

    refractored the check for correct pps format

--- RefractoringAssignment/src/EmployeeDetails.java
+++ RefractoringAssignment/src/EmployeeDetails.java
@@ -654,23 +654,23 @@ public class EmployeeDetails extends JFrame implements ActionListener, ItemListe
 		boolean ppsExist = false;
 		// check for correct PPS format based on assignment description
 		if (pps.length() == 8 || pps.length() == 9) {
-			if (Character.isDigit(pps.charAt(0)) && Character.isDigit(pps.charAt(1))
-					&& Character.isDigit(pps.charAt(2))	&& Character.isDigit(pps.charAt(3)) 
-					&& Character.isDigit(pps.charAt(4))	&& Character.isDigit(pps.charAt(5)) 
-					&& Character.isDigit(pps.charAt(6))	&& Character.isLetter(pps.charAt(7))
-					&& (pps.length() == 8 || Character.isLetter(pps.charAt(8)))) {
-				// open file for reading
-				application.openReadFile(file.getAbsolutePath());
-				// look in file is PPS already in use
-				ppsExist = application.isPpsExist(pps, currentByte);
-				application.closeReadFile();// close file for reading
-			} // end if
-			else
-				ppsExist = true;
-		} // end if
-		else
-			ppsExist = true;
-
+			for (int i = 0; i < pps.length(); i++) {
+				if((Character.isDigit(pps.charAt(i)) && i !=pps.length()) || (Character.isLetter(pps.charAt(i)) && i ==(pps.length()-1))){
+					ppsExist = true;
+				}else {
+					ppsExist = false;
+					break;
+				}
+			}
+		}
+		
+		if(ppsExist == true) {
+			// open file for reading
+			application.openReadFile(file.getAbsolutePath());
+			// look in file is PPS already in use
+			ppsExist = application.isPpsExist(pps, currentByte);
+			application.closeReadFile();// close file for reading
+		}
 		return ppsExist;
 	}// end correctPPS
 
